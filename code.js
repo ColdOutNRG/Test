f0f283704efa673baa6b64f66e823491a25b8090
ColdOutNRG <coldoutlands@gmail.com>
2025-02-13 16:28:37 -0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+// Select the button by its ID
+const myButton = document.getElementById("myButton");
+
+// Add a click event listener to the button
+myButton.addEventListener("click", function () {
+  alert("You clicked the button!");
+});
